3c71956c39341b13e50744e97d0120b0b9a316e1
Francisco Rocha <wallisonrocha2008@gmail.com>
2025-03-28 18:38:43 -0300

    feat: serializer general

--- src/main/scala/core/serializer/ProtobufSerializer.scala
+++ src/main/scala/core/serializer/ProtobufSerializer.scala
@@ -3,7 +3,7 @@ package core.serializer
 
 import org.apache.pekko.actor.ExtendedActorSystem
 import org.apache.pekko.serialization.SerializerWithStringManifest
-import org.interscity.htc.core.entity.event.SerializableEvent
+import org.interscity.htc.core.entity.Serializable
 
 import java.io.NotSerializableException
 import scala.util.{Failure, Success, Try}
@@ -12,33 +12,51 @@ class ProtobufSerializer(
   val system: ExtendedActorSystem
                         ) extends SerializerWithStringManifest {
 
-  override def identifier: Int = 27011998
+  override def identifier: Int = 16071977
 
   override def manifest(o: AnyRef): String = o.getClass.getName
 
   override def toBinary(o: AnyRef): Array[Byte] =
     o match {
-      case event: SerializableEvent => event.toByteArray
+      case ser: Serializable => ser.toByteArray
       case _ =>
         throw new IllegalArgumentException(
-          s"Cannot serialize object of type: ${o.getClass.getName}. It does not extend SerializableEvent."
+          s"Não é possível serializar [${o.getClass.getName}] com ProtobufSerializer. " +
+            s"Ele não estende ${classOf[Serializable].getName}"
         )
     }
+  override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = {
+    val triedClazz: Try[Class[_ <: Serializable]] =
+      system.dynamicAccess.getClassFor[Serializable](manifest)
 
-  override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef =
-    system.dynamicAccess.getClassFor[SerializableEvent](manifest) match {
+    triedClazz match {
       case Success(clazz) =>
-        if (classOf[SerializableEvent].isAssignableFrom(clazz)) {
-          val parseFromMethod = clazz.getMethod("parseFrom", classOf[Array[Byte]])
-          parseFromMethod.invoke(null, bytes).asInstanceOf[SerializableEvent]
-        } else {
-          throw new NotSerializableException(
-            s"Unimplemented deserialization of message with manifest [$manifest] in [${getClass.getName}]"
-          )
+        Try {
+          val companionClassName = clazz.getName + "$"
+          val companionClass = system.dynamicAccess.getClassFor[AnyRef](companionClassName).get
+          val moduleField = companionClass.getField("MODULE$")
+          val companionObject = moduleField.get(null)
+
+          val parseFromMethod = companionObject.getClass.getMethod("parseFrom", classOf[Array[Byte]])
+
+          parseFromMethod.invoke(companionObject, bytes)
+
+        } match {
+          case Success(instance: AnyRef) => instance
+          case Failure(e) =>
+            val ex = new NotSerializableException(s"Falha ao invocar parseFrom para o manifesto [$manifest]")
+            ex.initCause(e)
+            throw ex
+          case Success(other) =>
+            throw new NotSerializableException(s"parseFrom retornou um tipo inesperado [$other] para o manifesto [$manifest]")
+
         }
       case Failure(exception) =>
-        throw new NotSerializableException(
-          s"Error deserializing message with manifest [$manifest]: ${exception.getMessage}"
-        )
+        throw {
+          val ex = new NotSerializableException(s"Não foi possível carregar a classe para o manifesto [$manifest]: ${exception.getMessage}")
+          ex.initCause(exception)
+          ex
+        }
     }
+  }
 }
