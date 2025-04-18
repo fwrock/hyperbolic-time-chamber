bb1c12d6c172cf436e26042ca7ffbfd0473c609a
Francisco Rocha <wallisonrocha2008@gmail.com>
2025-04-17 21:35:12 -0300

    chore: json report

--- src/main/scala/core/actor/manager/report/JsonReportData.scala
+++ src/main/scala/core/actor/manager/report/JsonReportData.scala
@@ -1,22 +1,24 @@
 package org.interscity.htc
 package core.actor.manager.report
 
+import core.entity.event.control.report.ReportEvent
+
 import org.apache.pekko.actor.ActorRef
-import org.interscity.htc.core.entity.event.control.report.ReportEvent
+import org.interscity.htc.core.util.JsonUtil
 
 import java.io.{BufferedWriter, FileWriter}
 import java.nio.file.{Files, Path, Paths}
 import scala.collection.mutable
 
-class CsvReportData(override val reportManager: ActorRef)
+class JsonReportData(override val reportManager: ActorRef)
     extends ReportData(
-      id = "csv-report-manager",
+      id = "json-report-manager",
       reportManager = reportManager
     ) {
 
-  private val prefix = Some(config.getString("htc.report-manager.csv.prefix")).getOrElse("report_")
-  private val directory = Some(config.getString("htc.report-manager.csv.directory")).getOrElse(s"/tmp/reports/csv/${System.currentTimeMillis()}")
-  private val batchSize = Some(config.getInt("htc.report-manager.csv.batch-size")).getOrElse(1000)
+  private val prefix = Some(config.getString("htc.report-manager.json.prefix")).getOrElse("report_")
+  private val directory = Some(config.getString("htc.report-manager.json.directory")).getOrElse(s"/tmp/reports/json/${System.currentTimeMillis()}")
+  private val batchSize = Some(config.getInt("htc.report-manager.json.batch-size")).getOrElse(1000)
 
   private val buffer = mutable.ListBuffer[ReportEvent]()
   private var fileWriter: Option[BufferedWriter] = None
@@ -30,11 +32,11 @@ class CsvReportData(override val reportManager: ActorRef)
 
   private def flushBuffer(): Unit = {
     mkdir(directory)
-    val filePath = s"$directory/$prefix${System.currentTimeMillis()}.csv"
+    val filePath = s"$directory/$prefix${System.currentTimeMillis()}.json"
     val writer = new BufferedWriter(new FileWriter(filePath, true))
     buffer.foreach {
       report =>
-        writer.write(s"${report.entityId},${report.timestamp},${report.tick}${report.lamportTick},${report.data}\n")
+        writer.write(JsonUtil.toJson(buffer))
     }
     writer.close()
     buffer.clear()
