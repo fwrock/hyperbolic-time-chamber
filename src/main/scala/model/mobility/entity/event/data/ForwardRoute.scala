c80cfc7021c6025d7ae83ed1f6cd726f7f1992a9
Francisco Rocha <wallisonrocha2008@gmail.com>
2025-04-25 11:43:17 -0300

    chore: ForwardRoute add path

--- src/main/scala/model/mobility/entity/event/data/ForwardRoute.scala
+++ src/main/scala/model/mobility/entity/event/data/ForwardRoute.scala
@@ -1,4 +1,10 @@
 package org.interscity.htc
 package model.mobility.entity.event.data
 
-case class ForwardRoute()
+import org.htc.protobuf.core.entity.actor.Identify
+
+import scala.collection.mutable
+
+case class ForwardRoute(
+  path: Option[mutable.Queue[(Identify, Identify)]] = None,
+                       )
