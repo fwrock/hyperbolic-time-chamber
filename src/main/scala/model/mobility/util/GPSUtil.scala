dfeea0e9b9ac2356dfebf420d8df5c9e6ffc233d
Francisco Rocha <wallisonrocha2008@gmail.com>
2025-05-31 08:31:39 -0300

    chore: gps util with cache

--- src/main/scala/model/mobility/util/GPSUtil.scala
+++ src/main/scala/model/mobility/util/GPSUtil.scala
@@ -1,10 +1,47 @@
 package org.interscity.htc.model.mobility.util
 
-// Importa NodeGraph e EdgeGraph do novo pacote em Model.scala
+import org.interscity.htc.core.util.JsonUtil
+import org.interscity.htc.core.util.JsonUtil.writeJsonBytes
 import org.interscity.htc.model.mobility.entity.state.model.{EdgeGraph, NodeGraph}
+import org.interscity.htc.system.database.redis.RedisClientManager
+
+import java.util.UUID
 import scala.collection.mutable
 
 object GPSUtil {
+
+
+  def calcRoute(redisManager: RedisClientManager, originId: String, destinationId: String): Option[(Double, mutable.Queue[(String, String)])] = {
+
+    val routeId = UUID.nameUUIDFromBytes(s"route:${originId}-${destinationId}".getBytes).toString
+    redisManager.load(routeId) match
+      case Some(route) =>
+        Some(JsonUtil.fromJsonBytes[(Double, mutable.Queue[(String, String)])](route))
+      case None =>
+        val originNodeOpt = CityMapUtil.nodesById.get(originId)
+        val destinationNodeOpt = CityMapUtil.nodesById.get(destinationId)
+        (originNodeOpt, destinationNodeOpt) match {
+          case (Some(originNode), Some(destinationNode)) =>
+            CityMapUtil.cityMap.aStarEdgeTargetsOptimized(originNode, destinationNode, heuristicFunc) match {
+              case Some((cost, path)) =>
+                val routeQueue = mutable.Queue[(String, String)]()
+                path.foreach { case (edgeObject, targetNodeOfEdgeInPath) =>
+                  routeQueue.enqueue((edgeObject.label.id, targetNodeOfEdgeInPath.id))
+                }
+                redisManager.save(routeId, writeJsonBytes[(Double, mutable.Queue[(String, String)])]((cost, routeQueue)))
+                Some((cost, routeQueue))
+              case None =>
+                System.err.println(s"GPSUtil: Nenhuma rota encontrada de $originId para $destinationId.")
+                None 
+            }
+          case (None, _) =>
+            System.err.println(s"GPSUtil: Nó de origem $originId não encontrado no mapa.")
+            None
+          case (_, None) =>
+            System.err.println(s"GPSUtil: Nó de destino $destinationId não encontrado no mapa.")
+            None 
+        }
+  }
 
   /**
    * Calcula a rota entre dois nós usando A*.
@@ -15,13 +52,11 @@ object GPSUtil {
    * @return Option contendo (custo, fila de rota) ou None se a rota não for encontrada.
    */
   def calcRoute(originId: String, destinationId: String): Option[(Double, mutable.Queue[(String, String)])] = {
-    // Busca nós de origem e destino usando o mapa de consulta rápida O(1)
     val originNodeOpt = CityMapUtil.nodesById.get(originId)
     val destinationNodeOpt = CityMapUtil.nodesById.get(destinationId)
 
     (originNodeOpt, destinationNodeOpt) match {
       case (Some(originNode), Some(destinationNode)) =>
-        // Usa o A* da classe Graph, que retorna (Custo, Lista de (Aresta, NóDestino))
         CityMapUtil.cityMap.aStarEdgeTargetsOptimized(originNode, destinationNode, heuristicFunc) match {
           case Some((cost, path)) => // path é List[(Edge[NodeGraph, Double, EdgeGraph], NodeGraph)]
             val routeQueue = mutable.Queue[(String, String)]()
