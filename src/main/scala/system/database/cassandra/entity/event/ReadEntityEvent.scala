1085752e5f2a0c1305c793c1ebc5d5d49ce5a9bf
Francisco Rocha <wallisonrocha2008@gmail.com>
2025-08-26 09:49:49 -0300

    feat: add cassandra configurations

--- src/main/scala/system/database/cassandra/entity/event/ReadEntityEvent.scala
+++ src/main/scala/system/database/cassandra/entity/event/ReadEntityEvent.scala
@@ -4,7 +4,8 @@ package system.database.cassandra.entity.event
 import org.interscity.htc.system.entity.event.BaseEvent
 
 case class ReadEntityEvent(
-  table: String,
-  projection: String,
-  selection: String
-) extends BaseEvent
+                            table: String,
+                            projection: String,
+                            selection: String, // A seleção ainda pode ser uma string, mas os valores serão passados separadamente
+                            selectionArgs: List[Any],
+                          ) extends BaseEvent
