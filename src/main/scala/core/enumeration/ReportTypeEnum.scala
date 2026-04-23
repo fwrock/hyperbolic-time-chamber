f14320933bef0e0a7ed7cfded3ea107307541a8e
Francisco Wallison <wallisonrocha2008@gmail.com>
2026-04-22 23:07:58 -0300

    chore: click house report

--- src/main/scala/core/enumeration/ReportTypeEnum.scala
+++ src/main/scala/core/enumeration/ReportTypeEnum.scala
@@ -1,9 +1,15 @@
 package org.interscity.htc
 package core.enumeration
 
-import org.interscity.htc.core.actor.manager.report.{ CsvReportData, JsonReportData, ReportData }
+import org.interscity.htc.core.actor.manager.report.{
+  ClickHouseReportData,
+  CsvReportData,
+  JsonReportData,
+  ReportData
+}
 
 enum ReportTypeEnum(val clazz: Class[? <: ReportData]) {
-  case csv extends ReportTypeEnum(classOf[CsvReportData])
-  case json extends ReportTypeEnum(classOf[JsonReportData])
+  case csv        extends ReportTypeEnum(classOf[CsvReportData])
+  case json       extends ReportTypeEnum(classOf[JsonReportData])
+  case clickhouse extends ReportTypeEnum(classOf[ClickHouseReportData])
 }
